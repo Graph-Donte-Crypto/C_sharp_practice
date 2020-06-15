1bcc4aa9ba460135de03de3fdf3cdcfe97d69ff9
DAbraztsou <DAbraztsou@gomel.iba.by>
2020-06-15 11:05:15 +0300

    Task 2

--- Student.cs
+++ Student.cs
@@ -1,13 +1,53 @@
-﻿public class Student {
+﻿using System;
+
+public class Student {
 	string name;
 	string group;
+	string curator;
+	public enum StudyYear { 
+		First,
+		Second,
+		Third,
+		Fourth
+	};
+	StudyYear studyYear;
+	bool isPaidEducation = true;
 	public Mark[] marks;
+	public Student() { }
+
+	public Student(string name, string group, StudyYear studyYear) {
+		this.name = name;
+		this.group = group;
+		this.studyYear = studyYear;
+	}
 	public double GetAvgMark() {
 		int sum = 0;
 		foreach (Mark mark in marks) {
 			sum += mark.value;
 		}
 		return ((double)sum) / marks.Length;
+	}
+
+	public void setDefaultCurator() {
+		switch (studyYear) {
+			case StudyYear.First:
+				curator = null;
+				break;
+			case StudyYear.Second:
+				curator = "Петренко";
+				break;
+			default:
+				curator = "Мясников";
+				break;
+		}
+	}
+
+	public void TryToFixGrades() {
+		if (isPaidEducation) {
+			foreach (Mark mark in marks)
+				if (mark.value < 4)
+					mark.value = 4;
+		}
 	}
 	public void ResetAllMarks() {
 		foreach (Mark mark in marks)
